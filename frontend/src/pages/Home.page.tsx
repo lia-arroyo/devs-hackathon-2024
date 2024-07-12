e3a5bd736c295b6ecc51904a2127e74170d1042f
Jolin <jche521@aucklanduni.ac.nz>
2024-07-13 11:38:01 +1200

    rename demo components

--- frontend/src/pages/Home.page.tsx
+++ frontend/src/pages/Home.page.tsx
@@ -1,5 +1,5 @@
-import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { Welcome } from '../components/Demo_Welcome/Welcome';
+import { ColorSchemeToggle } from '../components/Demo_ColorSchemeToggle/ColorSchemeToggle';
 
 export function HomePage() {
   return (
