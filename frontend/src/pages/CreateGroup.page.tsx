bb2bfae07f65c9113ec2716d28e09b814dbaefce
Job Josef Santos <job.santos924@gmail.com>
2024-07-13 18:51:34 +1200

    feat: basic create group page done

--- frontend/src/pages/CreateGroup.page.tsx
+++ frontend/src/pages/CreateGroup.page.tsx
@@ -1,7 +1,14 @@
 import classes from './Pages.module.css';
+import { CreateGroupForm } from '@/components/CreateGroup/CreateGroupForm';
+import BubblyBackground from '@/components/BubbleBackground/BubbleBackground';
 
 const CreateGroupPage = () => {
-  return <div className={classes.pageContainer}>create group</div>;
+  return (
+    <div className={classes.pageContainer}>
+      <BubblyBackground />
+      <CreateGroupForm />
+    </div>
+  );
 };
 
 export default CreateGroupPage;
