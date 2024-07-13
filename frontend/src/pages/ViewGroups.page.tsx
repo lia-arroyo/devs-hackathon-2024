a8b823f605fecb1a8f46cf3b326d15abdb122c83
fsit869 <62918263+fsit869@users.noreply.github.com>
2024-07-14 00:22:01 +1200

    Signout button working

--- frontend/src/pages/ViewGroups.page.tsx
+++ frontend/src/pages/ViewGroups.page.tsx
@@ -2,6 +2,7 @@ import BubblyBackground from '@/components/BubbleBackground/BubbleBackground';
 import classes from './Pages.module.css';
 import GroupsContainer from '@/components/GroupsContainer/GroupsContainer';
 import {CheckLogin} from "@/components/CheckLogin/CheckLogin";
+import {SignoutButton} from "@/components/SignoutButton/SignoutButton";
 
 const ViewGroupsPage = () => {
   return (
@@ -9,6 +10,7 @@ const ViewGroupsPage = () => {
       <CheckLogin/>
       <BubblyBackground />
       <GroupsContainer />
+      <SignoutButton/>
     </div>
   );
 };
