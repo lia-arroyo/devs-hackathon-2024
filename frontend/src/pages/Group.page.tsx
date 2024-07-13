509f4b7993827340fd2823244470037ffec396a4
Jolin Chen <jche521@aucklanduni.ac.nz>
2024-07-13 18:29:19 +1200

    group page

--- frontend/src/pages/Group.page.tsx
+++ frontend/src/pages/Group.page.tsx
@@ -1,7 +1,37 @@
+import { BackgroundImage, Button, Flex, Group, Select, Text, Title } from '@mantine/core';
 import classes from './Pages.module.css';
+import bg from '../assets/background.png';
+import { useState } from 'react';
+import { IconBrandTrello, IconPlus } from '@tabler/icons-react';
 
 const GroupPage = () => {
-  return <div className={classes.pageContainer}>view group</div>;
+  const data = ["Jolin's", "Lia's"];
+  const [value, setValue] = useState<string | string>();
+  return (
+    <div className={classes.pageContainer}>
+      <BackgroundImage src={bg} className={classes.background}>
+        <Text size="sm" color="navyBlue.8">
+          Group Name
+        </Text>
+        <Title order={2} c="navyBlue.8">
+          #321543
+        </Title>
+        <div>
+          <Text size="md" color="navyBlue.8">
+            You are currently#1
+          </Text>
+        </div>
+        <Group mt={10}>
+          <Button leftSection={<IconPlus size={20} />} variant="gradient" size="sm">
+            Record Drink
+          </Button>
+          <Button leftSection={<IconBrandTrello size={20} />} variant="gradient" size="sm">
+            Leaderboard
+          </Button>
+        </Group>
+      </BackgroundImage>
+    </div>
+  );
 };
 
 export default GroupPage;
